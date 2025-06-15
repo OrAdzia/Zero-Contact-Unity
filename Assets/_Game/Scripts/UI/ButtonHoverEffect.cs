98b528d719292c88ed8ec577968ced2441834199
OrAdzia <oradzia.work@gmail.com>
2025-06-15 18:32:36 +0300

    Main Menu Gamepad Support
    Added the ability to use a gamepad in the main menu.

--- Assets/_Game/Scripts/UI/ButtonHoverEffect.cs
+++ Assets/_Game/Scripts/UI/ButtonHoverEffect.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Vector3 originalScale;
     public float hoverScale = 1.1f;
     public float scaleSpeed = 10f;
+    public float selectedScale = 1.1f;
+
+    private Vector3 originalScale;
     private Vector3 targetScale;
+    private Button button;
 
     void Start()
     {
         originalScale = transform.localScale;
         targetScale = originalScale;
+        button = GetComponent<Button>();
     }
 
     void Update()
     {
+        if (EventSystem.current.currentSelectedGameObject == gameObject)
+            targetScale = originalScale * selectedScale;
+        else
+            targetScale = originalScale;
+
         transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * scaleSpeed);
     }
 
